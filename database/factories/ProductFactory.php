c11c6a75b98869bf8b7d7acb3828ac5112f05a28
Christophe <cdo_outeiro@me.com>
2020-05-12 13:14:25 +0000

    Apply fixes from StyleCI

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -9,12 +9,12 @@ $factory->define(Product::class, function (Faker $faker) {
     return [
         'name' => $name = $faker->words(3, true),
         'slug' => str_slug($name),
-        'category_id' => $faker->numberBetween(1,20),
-        'collection_id' => $faker->numberBetween(1,20),
+        'category_id' => $faker->numberBetween(1, 20),
+        'collection_id' => $faker->numberBetween(1, 20),
         'description' => $faker->paragraph,
         'tax_id' => 1,
-        'price' => $faker->randomFloat(2,5,500),
-        'stock' => $faker->numberBetween(0,5),
+        'price' => $faker->randomFloat(2, 5, 500),
+        'stock' => $faker->numberBetween(0, 5),
         'enabled' => $faker->boolean(90),
     ];
 });
