3f1092728a7c43f0e3d5ba3c45cfb8ad1e689e96
Christophe <cdo_outeiro@me.com>
2020-05-19 08:18:16 +0000

    Apply fixes from StyleCI

--- src/Traits/Products/HasCart.php
+++ src/Traits/Products/HasCart.php
@@ -2,36 +2,35 @@
 
 namespace Suavy\LojaForLaravel\Traits\Products;
 
-use Illuminate\Support\Str;
-
 trait HasCart
 {
-
     public function cartAdd($quantity)
     {
-        \Cart::session(session()->getId())->add(array(
+        \Cart::session(session()->getId())->add([
             'id' => $this->id, // inique row ID
             'name' => $this->name,
             'price' => $this->price,
             'quantity' => $quantity,
-            'associatedModel' => $this
-        ));
+            'associatedModel' => $this,
+        ]);
     }
 
-    public function cartRemove(){
+    public function cartRemove()
+    {
         \Cart::session(session()->getId())->remove($this->id);
     }
 
-    public function cartAddQuantity(){
-        \Cart::session(session()->getId())->update($this->id, array(
-            'quantity' => +1
-        ));
+    public function cartAddQuantity()
+    {
+        \Cart::session(session()->getId())->update($this->id, [
+            'quantity' => +1,
+        ]);
     }
 
-    public function cartLessQuantity(){
-        \Cart::session(session()->getId())->update($this->id, array(
-            'quantity' => -1
-        ));
+    public function cartLessQuantity()
+    {
+        \Cart::session(session()->getId())->update($this->id, [
+            'quantity' => -1,
+        ]);
     }
-
 }
