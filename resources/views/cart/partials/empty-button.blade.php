61550dc6215e40f189b76ff53318f19abd058e8b
breno romeiro <bsromeiro@gmail.com>
2020-05-28 22:17:51 -0300

    Added buttonclasse on buttons

--- resources/views/cart/partials/empty-button.blade.php
+++ resources/views/cart/partials/empty-button.blade.php
@@ -1,5 +1,5 @@
 <div class="cart-empty-button-container">
-    <a href="{{ route('loja.cart.empty') }}" class="cart-empty-button js-empty-cart">
+    <a href="{{ route('loja.cart.empty') }}" class="button cart-empty-button js-empty-cart">
         <i class="fad fa-trash-alt cart-empty-button__icon"></i>
         <span class="cart-empty-button__text">Vider mon panier</span>
     </a>
--- resources/views/cart/partials/show-button.blade.php
+++ resources/views/cart/partials/show-button.blade.php
@@ -1,4 +1,4 @@
-<div class="cart-show-button-container">
+<div class="button cart-show-button-container">
     <a href="{{ route('loja.cart.index') }}" class="cart-show-button">
         <i class="fad fa-shopping-cart cart-show-button__icon"></i>
         <span class="cart-show-button__count" id="js-cart-quantity">{{ \Cart::session(session()->getId())->getTotalQuantity() }}</span>
