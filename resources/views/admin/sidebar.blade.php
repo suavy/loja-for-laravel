7aa297a7997627c63310e37586e64b0e2faa60b2
Christophe <cdo_outeiro@me.com>
2020-06-04 16:14:09 +0200

    Sidebar + settings migration

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,1 +1,7 @@
 <!-- Loja (backpack) sidebar -->
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('product') }}"><i class="la la-tag nav-icon"></i> Produits</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('country-delivery') }}"><i class="la la-tag nav-icon"></i> Pays de livraison</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('category') }}"><i class="la la-sitemap nav-icon"></i> Catégories</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('collection') }}"><i class="la la-tag nav-icon"></i> Collections</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('tax') }}"><i class="la la-percentage nav-icon"></i> Taxes</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('setting') }}"><i class="la la-cog nav-icon"></i> Settings</a></li>
