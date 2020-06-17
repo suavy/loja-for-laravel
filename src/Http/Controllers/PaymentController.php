ff94cdeba391cf6d0d6aca04f801c6081182914c
Christophe <cdo_outeiro@me.com>
2020-06-17 09:09:54 +0000

    Apply fixes from StyleCI

--- src/Http/Controllers/PaymentController.php
+++ src/Http/Controllers/PaymentController.php
@@ -2,20 +2,16 @@
 
 namespace Suavy\LojaForLaravel\Http\Controllers;
 
-use Former\Facades\Former;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Suavy\LojaForLaravel\Http\Requests\PaymentRequest;
-use Suavy\LojaForLaravel\Models\Address;
-use Suavy\LojaForLaravel\Models\Product;
 
 class PaymentController extends Controller
 {
-
     public function index(PaymentRequest $request)
     {
         if ($request->has('address')) {
             Auth::user()->updateAddress($request->input('address'));
+
             return back();
         }
 
@@ -28,6 +24,4 @@ class PaymentController extends Controller
             return view('loja::cart.payment', compact('cartItems'));
         }
     }
-
-
 }
