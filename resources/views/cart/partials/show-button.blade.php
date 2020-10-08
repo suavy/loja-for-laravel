0a00781db1c7184b168c44e8da7f00145b01772b
Your Name <bsromeiro@gmail.com>
2020-10-08 20:44:00 -0300

    add : user button on navbar

--- resources/views/cart/partials/show-button.blade.php
+++ resources/views/cart/partials/show-button.blade.php
@@ -1,5 +1,8 @@
 <div class="button cart-show-button-container">
     <a href="{{ route('loja.cart.index') }}" class="cart-show-button">
+        <i class="fas fa-user cart-show-button__icon"></i>
+    </a>
+    <a href="{{ route('loja.cart.index') }}" class="cart-show-button">
         <i class="fas fa-shopping-bag cart-show-button__icon"></i>
         <span class="cart-show-button__count" id="js-cart-quantity">{{ \Cart::session(session()->getId())->getTotalQuantity() }}</span>
     </a>
