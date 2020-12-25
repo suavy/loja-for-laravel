2dbce21874f5af8c90e45cc79afa7f89ed7ee0f1
Christophe <cdo9@users.noreply.github.com>
2020-12-25 14:35:26 +0000

    Apply fixes from StyleCI

--- routes/web.php
+++ routes/web.php
@@ -11,39 +11,39 @@ use Suavy\LojaForLaravel\Http\Controllers\ProductController;
 use Suavy\LojaForLaravel\Http\Controllers\SearchController;
 
 Route::group([
-        'middleware' => 'web',
-        'prefix' => config('loja.prefix'),
-    ], function () {
-    Route::get('/', [HomeController::class, 'index'])->name('loja.home');
+    'middleware' => 'web',
+    'prefix' => config('loja.prefix'),
+], function () {
+        Route::get('/', [HomeController::class, 'index'])->name('loja.home');
 
-    /*
-     * Product, Category, Collection routing
-     */
-    Route::get('/product/{product}', [ProductController::class, 'show'])->name('loja.product.show');
-    Route::get('/categories', [CategoryController::class, 'index'])->name('loja.category.index');
-    Route::get('/category/{id}', [CategoryController::class, 'show'])->name('loja.category.show');
-    Route::get('/collections', [CollectionController::class, 'index'])->name('loja.collection.index');
-    Route::get('/collection/{id}', [CollectionController::class, 'show'])->name('loja.collection.show');
-    Route::get('/search', [SearchController::class, 'index'])->name('loja.search');
+        /*
+         * Product, Category, Collection routing
+         */
+        Route::get('/product/{product}', [ProductController::class, 'show'])->name('loja.product.show');
+        Route::get('/categories', [CategoryController::class, 'index'])->name('loja.category.index');
+        Route::get('/category/{id}', [CategoryController::class, 'show'])->name('loja.category.show');
+        Route::get('/collections', [CollectionController::class, 'index'])->name('loja.collection.index');
+        Route::get('/collection/{id}', [CollectionController::class, 'show'])->name('loja.collection.show');
+        Route::get('/search', [SearchController::class, 'index'])->name('loja.search');
 
-    /*
-     * Order routing
-     */
-    Route::get('/user/orders', [OrderController::class, 'index'])->name('loja.order.index');
-    Route::get('/user/order/{id}', [OrderController::class, 'show'])->name('loja.order.show');
+        /*
+         * Order routing
+         */
+        Route::get('/user/orders', [OrderController::class, 'index'])->name('loja.order.index');
+        Route::get('/user/order/{id}', [OrderController::class, 'show'])->name('loja.order.show');
 
-    /*
-     * Cart routing
-     */
-    Route::get('/cart', [CartController::class, 'index'])->name('loja.cart.index');
-    Route::post('/cart/add/{product}', [CartController::class, 'productAdd'])->name('loja.cart.product.add');
-    Route::post('/cart/update/{product}', [CartController::class, 'productUpdateQuantity'])->name('loja.cart.product.update.quantity');
-    Route::post('/cart/remove/{product}', [CartController::class, 'productRemove'])->name('loja.cart.product.remove');
-    Route::get('/cart/empty', [CartController::class, 'empty'])->name('loja.cart.empty');
+        /*
+         * Cart routing
+         */
+        Route::get('/cart', [CartController::class, 'index'])->name('loja.cart.index');
+        Route::post('/cart/add/{product}', [CartController::class, 'productAdd'])->name('loja.cart.product.add');
+        Route::post('/cart/update/{product}', [CartController::class, 'productUpdateQuantity'])->name('loja.cart.product.update.quantity');
+        Route::post('/cart/remove/{product}', [CartController::class, 'productRemove'])->name('loja.cart.product.remove');
+        Route::get('/cart/empty', [CartController::class, 'empty'])->name('loja.cart.empty');
 
-    /*
-     * Payment routing
-     */
-    Route::post('/payment', [PaymentController::class, 'index'])->name('loja.payment.index');
-    Route::post('/charge', [PaymentController::class, 'charge'])->name('loja.payment.charge');
-});
+        /*
+         * Payment routing
+         */
+        Route::post('/payment', [PaymentController::class, 'index'])->name('loja.payment.index');
+        Route::post('/charge', [PaymentController::class, 'charge'])->name('loja.payment.charge');
+    });
