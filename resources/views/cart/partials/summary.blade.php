f32775eec05496dceea232ee4efd01049a40d782
vagrant <vagrant@homestead>
2021-02-04 11:53:23 +0000

    changing route name from loja.cart.payment to loja.payment.index

--- resources/views/cart/partials/summary.blade.php
+++ resources/views/cart/partials/summary.blade.php
@@ -6,14 +6,14 @@
     </div>
     @guest
         <div class="cart-summary__holder">
-            <a href="{{ route('loja.cart.payment') }}" class="button cart-summary-button cart-summary-button--checkout cart-summary-button--disabled">Paiement</a>
+            <a href="{{ route('loja.payment.index') }}" class="button cart-summary-button cart-summary-button--checkout cart-summary-button--disabled">Paiement</a>
         </div>
         <div class="cart-summary__holder">
             <p class="cart-summary__subscription"><a href="#">Connectez-vous</a> ou <a href="#">inscrivez-vous</a> pour pouvoir remplir vos informations de livraison et procéder au paiement.</p>
         </div>
     @elseguest
         <div class="cart-summary__holder">
-            <a href="{{ route('loja.cart.payment') }}" class="button cart-summary-button cart-summary-button--checkout">Paiement</a>
+            <a href="{{ route('loja.payment.index') }}" class="button cart-summary-button cart-summary-button--checkout">Paiement</a>
         </div>
         <div class="cart-summary__holder">
             <p>Nous acceptons : <i class="fab fa-cc-visa"></i> <i class="fab fa-cc-mastercard"></i> <i class="fab fa-cc-amex"></i></p>
