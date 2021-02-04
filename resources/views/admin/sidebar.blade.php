6e12afad64c9960875d6117514827b878f5f013c
vagrant <vagrant@homestead>
2021-02-04 14:16:55 +0000

    updating icons in sidebar

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,6 +1,6 @@
 <!-- Loja (backpack) sidebar -->
 <li class="nav-title">LOJA</li>
-<li class="nav-item"><a class="nav-link" href="{{ backpack_url('product') }}"><i class="la la-tag nav-icon"></i> Produits</a></li>
+<li class="nav-item"><a class="nav-link" href="{{ backpack_url('product') }}"><i class="nav-icon fad fa-tags"></i> Produits</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('country-delivery') }}"><i class="la la-tag nav-icon"></i> Pays de livraison</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('category') }}"><i class="la la-sitemap nav-icon"></i> Catégories</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('collection') }}"><i class="la la-tag nav-icon"></i> Collections</a></li>
@@ -10,4 +10,3 @@
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('attribute') }}"><i class="la la-cog nav-icon"></i> Attributs</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('attribute-value') }}"><i class="la la-cog nav-icon"></i> Valeurs d'attribut</a></li>
 <li class="nav-item"><a class="nav-link" href="{{ backpack_url('attribute-set') }}"><i class="la la-cog nav-icon"></i> Sets d'attributs</a></li>
-
