75daca013c10d80b2d266d965e5e009ec118edc0
Matthieu <kassian.matthieu@gmail.com>
2021-04-21 20:12:26 +0200

    upgrade php8 ok

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -3,6 +3,8 @@
 namespace Suavy\LojaForLaravel\Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Suavy\LojaForLaravel\Models\Category;
+use Suavy\LojaForLaravel\Models\Collection;
 use Suavy\LojaForLaravel\Models\Product;
 
 class ProductFactory extends Factory
@@ -14,13 +16,25 @@ class ProductFactory extends Factory
         return [
             'name' => $name = $this->faker->words(3, true),
             'slug' => str_slug($name),
-            'category_id' => $this->faker->numberBetween(1, 20),
-            'collection_id' => $this->faker->numberBetween(1, 20),
             'description' => $this->faker->paragraph,
             'tax_id' => 1,
             'price' => $this->faker->randomFloat(2, 5, 500),
             'stock' => $this->faker->numberBetween(0, 5),
             'enabled' => $this->faker->boolean(90),
         ];
+    }
+
+    public function configure()
+    {
+        dd(5151);
+        return $this
+            ->afterMaking(function(Product $product){
+                $product->category()->associate(
+                    Category::query()->inRandomOrder()->first()
+                );
+                $product->collection()->associate(
+                    Collection::query()->inRandomOrder()->first()
+                );
+            });
     }
 }
