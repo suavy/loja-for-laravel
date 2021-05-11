1c933158abb05f525c1e407b4ec68497c341b498
Christophe Do Outeiro <cdo_outeiro@me.com>
2021-05-11 09:55:10 +0100

    change stripe/webhook to "GET"

--- routes/web.php
+++ routes/web.php
@@ -42,5 +42,5 @@ Route::group([
      * Payment routing
      */
     Route::post('/payment', [PaymentController::class, 'index'])->name('loja.payment.index');
-    Route::post('stripe/webhook', [PaymentController::class, 'webhook'])->name('loja.payment.webhook');
+    Route::get('stripe/webhook', [PaymentController::class, 'webhook'])->name('loja.payment.webhook');
 });
