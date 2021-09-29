c41fdf319db14d5a73cc570e47774987679bb9da
Christophe Do Outeiro <cdo_outeiro@me.com>
2021-09-29 15:53:16 +0100

    Mail translation + try fix select product show quantity

--- resources/views/livewire/cart-add-product.blade.php
+++ resources/views/livewire/cart-add-product.blade.php
@@ -5,16 +5,18 @@
 
     <div class="relative">
         <div class="text-center left-0 pt-2 right-0 absolute block text-xs uppercase text-gray-400 tracking-wide font-semibold">Qty</div>
-        <select class="cursor-pointer appearance-none rounded-xl border border-gray-200 pl-4 pr-8 h-14 flex items-end pb-1" wire:model="quantity">
+        <select name="quantity"id="quantity" class="cursor-pointer appearance-none rounded-xl border border-gray-200 pl-4 pr-8 h-14 flex items-end pb-1" wire:model="quantity">
 
             {{-- TODO USE config('loja.quantity.maximum-product'); --}}
             @for($i=1; $i<=5; $i++)
                 <option value ="{{ $i }}">{{ $i }}</option>
             @endfor
         </select>
-        <svg class="w-5 h-5 text-gray-400 absolute right-0 bottom-0 mb-2 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
-        </svg>
+        <label for="quantity">
+            <svg class="w-5 h-5 text-gray-400 absolute right-0 bottom-0 mb-2 mr-2" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 9l4-4 4 4m0 6l-4 4-4-4" />
+            </svg>
+        </label>
     </div>
 
     @if($product->hasAttributes())
